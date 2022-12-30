413e82228bf478e3540a6619b7351394059c3782
ScerioJones <james.liberto97@gmail.com>
2022-12-30 15:23:25 -0600

    first edit to connnect to api test

--- test.py
+++ test.py
@@ -2,5 +2,5 @@ import pandas as pd
 import numpy as np
 import requests
 
-print('hello world')
-print('it worked')
+response = requests.get("https://api.the-odds-api.com/v4/sports/upcoming/odds/?regions=us&markets=h2h&oddsFormat=american&apiKey=fa59214500c3ee27dd499bf436405160")
+print(response)
