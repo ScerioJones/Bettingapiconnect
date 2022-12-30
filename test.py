7db548fcd28c27a924697fe3561ff0e5b86abc46
ScerioJones <james.liberto97@gmail.com>
2022-12-30 16:06:25 -0600

    h

--- test.py
+++ test.py
@@ -6,5 +6,5 @@ import json
 response = requests.get("https://api.the-odds-api.com/v4/sports/upcoming/odds/?regions=us&markets=h2h&oddsFormat=american&apiKey=fa59214500c3ee27dd499bf436405160")
 jsonResponse = response.json()
 #print(jsonResponse)
-print(jsonResponse.keys)
+print(list(jsonResponse.keys))
 print('test')
