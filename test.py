019b4c490b8672301b456c47f9e67cb1da73878b
ScerioJones <james.liberto97@gmail.com>
2022-12-30 15:39:18 -0600

    test1

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
 import requests
+import json
 
 response = requests.get("https://api.the-odds-api.com/v4/sports/upcoming/odds/?regions=us&markets=h2h&oddsFormat=american&apiKey=fa59214500c3ee27dd499bf436405160")
-jsonResponse = response.json()
+jsonResponse = json.loads(response.txt)
 print(jsonResponse)
+
